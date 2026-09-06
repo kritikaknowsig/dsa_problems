96c3ebeafe683d626d74e28defb5f00c9190d2db
kritika <kritikakushwaha133@gmail.com>
2026-09-06 22:17:39 +0530

    Time: 535 ms (5.40%) | Memory: 46.7 MB (82.00%) - LeetSync

---
+++ 136-single-number/single-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+       
+       
+        for (int i = 0; i < nums.length; i++) {
+            int n = 0;
+            for (int j = 0 ; j < nums.length; j++) {
+                if (nums[i] == nums[j]) {
+                    n++;
+                }
+
+            }
+            if (n == 1) {
+                return nums[i];
+            }
+        }
+        return 1;
+    }
+}
