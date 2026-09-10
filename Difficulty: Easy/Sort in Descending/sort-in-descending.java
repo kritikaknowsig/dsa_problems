7db8036ba44af46b67021a092766c1686137a5fc
kritika <kritikakushwaha133@gmail.com>
2026-09-10 16:58:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Sort in Descending/sort-in-descending.java
@@ -0,0 +1,16 @@
+class Solution {
+	public void sortInDesc(int[] arr) {
+		for (int i = 0; i < arr.length - 1; i++) {
+			int minindex = i;
+			for (int j = (i + 1) ; j < arr.length ; j++) {
+				if (arr[j]>arr[minindex]) {
+					minindex = j;
+				}
+			}
+			int temp = arr[i];
+			arr[i] = arr[minindex];
+			arr[minindex] = temp;
+		}
+		
+	}
+}
