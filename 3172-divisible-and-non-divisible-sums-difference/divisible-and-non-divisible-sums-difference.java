3b8b2d807cba62e717bb399043472c107a62206e
kritika <kritikakushwaha133@gmail.com>
2026-09-10 10:03:53 +0530

    Time: 1 ms (93.63%) | Memory: 42.3 MB (91.42%) - LeetSync

---
+++ 3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int differenceOfSums(int n, int m) {
+        int result = 0;
+        int nums1 = 0;
+        int nums2 = 0;
+        for(int i = 1; i<=n;i++){
+        if(i%m!=0){
+        nums1 +=i;
+        }else{
+        nums2 +=i;
+        }
+        
+        }result = (nums1-nums2);
+
+        return result;
+        
+    }
+}
